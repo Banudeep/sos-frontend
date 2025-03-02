16fd54e201b9d63ce42142bd58f638b8cdefcb91
Banudeep <60642712+Banudeep@users.noreply.github.com>
2025-03-02 00:31:39 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sos-app/",
   plugins: [react()],
+  build: {
+    outDir: "build", // Vercel looks for a "build" directory by default; this line is optional if you prefer a different folder name
+  },
 });
