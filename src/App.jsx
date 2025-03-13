a03b4755fb9bb2cc01f4d85161b83d8d3abde659
Banudeep <60642712+Banudeep@users.noreply.github.com>
2025-03-13 02:04:31 -0400

    ambulance

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import SOS_form from "./Components/SOS_form";
+import Ambulance from "./Components/ambulance";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sos" element={<SOS_form />} />
+          <Route path="/ambulance" element={<Ambulance />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>
